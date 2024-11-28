d30c7347e077f8ed468e21faeea933e6edcbc2b5
vpaul007 <96490757+vpaul007@users.noreply.github.com>
2024-11-28 11:43:56 +0000

    Create CustomerAccount.java

---
+++ CustomerAccount.java
@@ -0,0 +1,8 @@
+package Atmproject;
+
+public class CustomerAccount extends Account1 {
+
+	public CustomerAccount(int customerNumber, int pinNumber, double accountBalance) {
+		super(customerNumber, pinNumber, accountBalance);
+	
+	}
